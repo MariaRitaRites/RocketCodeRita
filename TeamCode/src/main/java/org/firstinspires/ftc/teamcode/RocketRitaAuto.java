9bba451b978afe8cf99a5831f6ae73bd66c7db5d
Rocket Striker Rita <marierites8@gmail.com>
2025-06-12 08:19:29 -0300

    primeira versão do auto rocketstriker

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RocketRitaAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RocketRitaAuto.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="RocketRitaAuto", group = "Robot")
-public class RocketRitaAuto {
+public class RocketRitaAuto extends LinearOpMode {
 
     private DcMotor FL0 = null;
     private DcMotor FR1 = null;
@@ -50,6 +50,59 @@ public class RocketRitaAuto {
        BR3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
        telemetry.addData("Starting at", "%7d :%7d",
+                        FL0.getCurrentPosition(),
+                        FR1.getCurrentPosition(),
+                        BL2.getCurrentPosition());
+                        BR3.getCurrentPosition());
+
+       telemetry.update();
+
+       waitForStart();
+
+       new RocketRitaAuto(DRIVE_SPEED, 48, 48, 5.0);
+       new RocketRitaAuto(TURN_SPEED, 12, -12, 4.0);
+       new RocketRitaAuto(DRIVE_SPEED, -24, -24, 4.0);
+
+       telemetry.addData( "Path", "Complete");
+       telemetry.update();
+       sleep(1000);
+
+       public void encoderDrive(double speed,
+                                double leftInches, double rightInches,
+                                double timeoutS) {
+           int newLeftTarget;
+           int newRightTarget;
+
+           if (OpModeIsActive()) {
+
+               newLeftTarget = FL0.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+               newLeftTarget = FR1.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+               newRightTarget = BL2.getCurrentPosition() + (int)(leftInches * COUNTS_PER_INCH);
+               newRightTarget = BR3.getCurrentPosition() + (int)(rightInches * COUNTS_PER_INCH);
+               FL0.setTargetPosition(newLeftTarget);
+               FR1.setTargetPosition(newRightTarget);
+               BL2.setTargetPosition(newLeftTarget);
+               BR3.setTargetPosition(newRightTarget);
+
+               FL0.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+               FR1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+               BL2.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+               BR3.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+               runtime.reset();
+               FL0.setPower(Math.abs(speed));
+               FR1.setPower(Math.abs(speed));
+               BL2.setPower(Math.abs(speed));
+               BR3.setPower(Math.abs(speed));
+
+               while (opModeIsActive() &&
+                       (runtime.seconds() < timeoutS) &&
+                       (FL0.isBusy() && FR1.isBusy())) {
+                     boolean b = (BL2.isBusy() && BR3.isBusy()))
+               }
+           }
+   }
+
 
 
 
